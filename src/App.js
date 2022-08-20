c8fe1ac8808e64b2212f3940a372cc1a12d43e0c
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-20 11:03:25 +0800

    create maincontext in app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './component/Card';
 import { useState } from "react";
+import { mainContext } from "./context/mainContext"
 
 function App() {
   const [data, setData] = useState({
@@ -8,9 +9,15 @@ function App() {
     avatar: "https://reqres.in/img/faces/7-image.jpg",
   })
 
+  const mainContextValue = {
+    data
+  }
+
   return (
     <div className="App">
+      <mainContext.Provider value = {mainContextValue}>
       <Card data={data}/>
+      </mainContext.Provider>
     </div>
   );
 }
